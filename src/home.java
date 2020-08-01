72936530ab47d78fd9162eb0e9eb8ab2dc1ac746
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-07-31 23:27:51 -0400

    Initial commit

---
+++ src/home.java
@@ -0,0 +1,24 @@
+/**
+ * Program Name: home.java
+ * Purpose: put something here
+ * Coder: Libo Chen
+ * Date: Jul. 31, 2020
+ */
+
+/**
+ * @author Libo
+ *
+ */
+public class home
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/stockModel/cutomerModel.java
@@ -0,0 +1,25 @@
+/**
+ * Program Name: cutomerModel.java
+ * Purpose: put something here
+ * Coder: Libo Chen
+ * Date: Jul. 31, 2020
+ */
+package stockModel;
+
+/**
+ * @author Libo
+ *
+ */
+public class cutomerModel
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
