2a4484f594bb6e29d257c7adfad774d3da3cc20d
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-08-03 22:52:06 -0400

    update

--- src/view/invoiceInForm.java
+++ src/view/invoiceInForm.java
@@ -6,11 +6,21 @@
  */
 package view;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 /**
  * @author Libo
  *
  */
-public class invoiceInForm
+public class invoiceInForm extends JFrame
 {
+	private JLabel titleLbl, companyNameLbl,productNameLbl,productDimensionLbl,priceLbl,totalPriceLbl;
+	private JTextField titleFld, companyNameFld,productNameFld,productDimensionFld,priceFld,totalPriceFld;
+	private JButton addBtn, clearBtn;
+	
+	
 
 }
--- src/view/mainView.java
+++ src/view/mainView.java
@@ -6,11 +6,35 @@
  */
 package view;
 
+import java.awt.Dimension;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
 /**
  * @author Libo
  *
  */
-public class mainView
+public class mainView extends JFrame
 {
+	public mainView()
+	{
+		super("BookKeeping");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1000, 500);
+		this.setLocationRelativeTo(null);
+		setResizable(false);
 
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		new mainView();
+	}
 }
