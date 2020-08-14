18a0d9efb44c5f60eaf00c06033bc4714f0132bb
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-08-13 23:30:13 -0400

    update

--- src/productModel/productModel.java
+++ src/productModel/productModel.java
@@ -12,5 +12,68 @@ package productModel;
  */
 public class ProductModel
 {
-	private String productName;
+	private String productName, productDimension;
+	private double productQuantity; 
+  private double unitPrice, totalPrice;
+  private String note;
+  
+  public ProductModel(String n, String d, double q, double up, double tp, String no)
+  {
+  	productName=n;
+  	productDimension=d;
+  	productQuantity=q;
+  	unitPrice=up;
+  	totalPrice=tp;
+  	note=no;
+  }
+  
+  public void setProductName(String n)
+	{
+  	productName=n;
+	}
+  public void setProductDimension(String d)
+	{
+  	productDimension=d;
+	}  
+  public void setProductQuantity(double q)
+	{
+  	productQuantity=q;
+	}
+  public void setUnitPrice(double up)
+	{
+  	unitPrice=up;
+	}
+  public void setTotalPrice(double tp)
+	{
+  	totalPrice=tp;
+	}
+  public void setNote(String no)
+	{
+  	note=no;
+	}
+
+  public String getProductName()
+	{
+		return productName;
+	}
+  public String getProductDimension()
+	{
+		return productDimension;
+	}
+  public double getProductQuantity()
+	{
+		return productQuantity;
+	}
+  public double getUnitPrice()
+	{
+		return unitPrice;
+	}
+  public double getTotalPrice()
+	{
+		return totalPrice;
+	}
+  public String getNote()
+	{
+		return note;
+	}
 }
--- src/stockModel/stockModel.java
+++ src/stockModel/stockModel.java
@@ -12,70 +12,7 @@ package stockModel;
  */
 public class StockModel
 {
+	
 
-	private String productName, productDimension;
-	private double productQuantity; 
-  private double unitPrice, totalPrice;
-  private String note;
-  
-  public StockModel(String n, String d, double q, double up, double tp, String no)
-  {
-  	productName=n;
-  	productDimension=d;
-  	productQuantity=q;
-  	unitPrice=up;
-  	totalPrice=tp;
-  	note=no;
-  }
-  
-  public void setProductName(String n)
-	{
-  	productName=n;
-	}
-  public void setProductDimension(String d)
-	{
-  	productDimension=d;
-	}  
-  public void setProductQuantity(double q)
-	{
-  	productQuantity=q;
-	}
-  public void setUnitPrice(double up)
-	{
-  	unitPrice=up;
-	}
-  public void setTotalPrice(double tp)
-	{
-  	totalPrice=tp;
-	}
-  public void setNote(String no)
-	{
-  	note=no;
-	}
-
-  public String getProductName()
-	{
-		return productName;
-	}
-  public String getProductDimension()
-	{
-		return productDimension;
-	}
-  public double getProductQuantity()
-	{
-		return productQuantity;
-	}
-  public double getUnitPrice()
-	{
-		return unitPrice;
-	}
-  public double getTotalPrice()
-	{
-		return totalPrice;
-	}
-  public String getNote()
-	{
-		return note;
-	}
 
 }
