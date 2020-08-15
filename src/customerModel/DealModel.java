4db07b238a37c4618e2e3ba7f78a8c2486b24ccb
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-08-14 23:40:35 -0400

    update

---
+++ src/customerModel/DealModel.java
@@ -0,0 +1,44 @@
+/**
+ * Program Name: DealModel.java
+ * Purpose: put something here
+ * Coder: Libo Chen
+ * Date: Aug. 14, 2020
+ */
+package customerModel;
+import java.util.ArrayList;
+
+import moneyModel.DebtModel;
+import productModel.ProductModel;
+/**
+ * @author Libo
+ *
+ */
+public class DealModel
+{
+	private ArrayList<ProductModel> productList;
+	private double price;
+	
+	public DealModel(ArrayList<ProductModel> pl, double p)
+	{
+		productList=pl;
+		price=p;
+	}
+
+	public void setProductList(ArrayList<ProductModel> pl)
+	{
+		productList=pl;
+	}
+	public void setPrice(double p)
+	{
+		price=p;
+	}
+	
+	public ArrayList<ProductModel> getProductList()
+	{
+		return productList;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
+}
