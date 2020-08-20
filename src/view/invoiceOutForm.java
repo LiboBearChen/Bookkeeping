fa1b610c1ff77e811b68eb9bd340002d043da354
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-08-19 23:39:19 -0400

    update

--- src/view/invoiceOutForm.java
+++ src/view/invoiceOutForm.java
@@ -11,13 +11,83 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 /**
  * @author Libo
  *
  */
-public class InvoiceOutForm extends JFrame
+public class InvoiceOutForm extends Application
 {
-	private JLabel titleLbl, companyNameLbl,productNameLbl,productDimensionLbl,priceLbl,totalPriceLbl;
-	private JTextField titleFld, companyNameFld,productNameFld,productDimensionFld,priceFld,totalPriceFld;
-	private JButton addBtn, clearBtn;
+	private Label titleLbl, companyNameLbl,productNameLbl,productDimensionLbl,priceLbl,totalPriceLbl;
+	private TextField titleFld, companyNameFld,productNameFld,productDimensionFld,priceFld,totalPriceFld;
+	private Button addBtn, clearBtn;
+	
+	public void init()
+	{
+		//opening a connection to a database, or populating a ComboBox
+	}
+	
+	public void start(Stage myStage)
+	{
+		ButtonListener buttonListener = new ButtonListener();
+		
+		addBtn = new Button("Add");
+		addBtn.setOnAction(buttonListener);
+		clearBtn = new Button("Clear");
+		clearBtn.setOnAction(buttonListener);
+
+		
+		HBox hBox = new HBox();
+		hBox.getChildren().addAll(addBtn,clearBtn);
+		
+		BorderPane rootPane = new BorderPane();
+		rootPane.getStyleClass().add("rootPane");
+		rootPane.setBottom(hBox);
+
+		Scene myScene = new Scene(rootPane, 400, 200);
+		myScene.getStylesheets().add("InvoiceForm.css");
+		
+		myStage.setScene(myScene);
+		myStage.setTitle("Invoice Out Form");    
+		myStage.show();	
+	}
+	
+	public void stop()
+	{
+		//
+	}//end stop
+	
+	private class ButtonListener implements EventHandler<ActionEvent>
+  {
+		@Override
+		public void handle(ActionEvent ev)
+		{
+
+			if(ev.getSource()== addBtn)
+			{
+
+			}
+			else if(ev.getSource() == clearBtn)
+			{
+
+			}
+
+		}
+  }
+	
+	public static void main(String[] args)
+	{
+		launch();
+	}
+
 }
