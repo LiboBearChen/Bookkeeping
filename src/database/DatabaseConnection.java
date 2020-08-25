3aa6a8193820a910bf94809ac2d45f2e4d2a8468
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-08-24 20:22:25 -0400

    update

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -29,5 +29,58 @@ public class DatabaseConnection
 		return connection;
 	}
 	
-	
 }
+
+public class Driver
+{
+
+	public static void main(String[] args) throws SQLException
+	{
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRslt = null;
+		
+	
+		try
+		{
+
+			myConn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:1234/demo?useSSL=false","root","password"
+					);
+			
+			myStmt = myConn.createStatement();
+			
+			myRslt = myStmt.executeQuery("SELECT * FROM customers");
+			
+			while(myRslt.next())
+			{
+
+				System.out.println(myRslt.getString(2) +
+						               ", " + myRslt.getString(3) +
+						               ", " + myRslt.getString(4) 
+						               );
+			}		
+					
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Exception: " + ex.getMessage());
+		}
+		
+		finally
+		{
+
+			if(myRslt != null)
+				myRslt.close();
+			if(myStmt != null)
+				myStmt.close();
+			if(myConn != null)
+				myConn.close();			
+		}
+		
+		System.out.println("**\nClose Database Connection");		
+
+	}
+
+}
+
